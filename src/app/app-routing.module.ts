238807e17d6040ab55c26832895143b18c7372ca
Nnamdi Echem <nnamdi.b@gmail.com>
2023-08-31 20:21:30 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutPageComponent },
+  { path: 'projects', component: ProjectsPageComponent },
+  { path: '', component: LandingPageComponent },
+  { path: '**', component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { MenuPageComponent } from './menu-page/menu-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    PagenotfoundComponent,
+    MenuPageComponent,
+    AboutPageComponent,
+    ProjectsPageComponent
   ],
   imports: [
     BrowserModule,
